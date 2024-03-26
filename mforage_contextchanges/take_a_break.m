e0cd72eea37a14157edbac6154ed17737844c755
kelly <getkellygarner@gmail.com>
2024-03-27 10:03:42 +1100

    added animal puns for breaks

--- mforage_contextchanges/take_a_break.m
+++ mforage_contextchanges/take_a_break.m
@@ -7,14 +7,26 @@ function [] = take_a_break(window, count_trials, ntrials, breaks, backRect, xCen
 % backRect = Rect for image
 
 Screen('TextStyle', window, 1);
-Screen('TextSize', window, 30); 
+Screen('TextSize', window, 30);
 instructions = ...
-   sprintf(['Great work! \n'...
-                 '%d/%d blocks complete\n'...
-                 'Take a break...you earned it\n\n'...
-                 'Press any key to continue.\n'], (count_trials/breaks), (ntrials/breaks));
+    sprintf(['Great work! \n'...
+    '%d/%d blocks complete\n'...
+    'Take a break...you earned it\n\n'...
+    'Press any key to continue.\n'], (count_trials/breaks), (ntrials/breaks));
 DrawFormattedText(window, instructions,'Center', screenYpixels*.1, [0 0 255]);
-im = imread(sprintf('break.jpeg'));
+
+%%%% read in the break image files and randomly select one
+pics = dir('breakphotos');
+hidden_idx = [];
+for ipic = 1:length(pics) % remove any pesky hidden files
+    if length(pics(ipic).name) < 5
+        hidden_idx = [hidden_idx, ipic];
+    end
+end
+pics(hidden_idx) = [];
+this_pic = randperm(length(pics), 1);
+this_pic_full_file = fullfile(pics(this_pic).folder, pics(this_pic).name);
+im = imread(this_pic_full_file);
 tex = Screen('MakeTexture', window, im);
 centeredRect = CenterRectOnPointd(backRect, xCenter, yCenter);
 Screen('DrawTexture', window, tex, [], centeredRect);
